01a3117ec2e531a158a8162e0dceed69341229c0
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2025-04-30 15:43:12 -0700

    fixed issue with rows

--- server.js
+++ server.js
@@ -22,12 +22,14 @@ const dbConfig = {
 
 app.get('/polygons', async (req, res) => {
   try {
+    let rows;
+    
     if (process.env.ENV === 'dev') {
       const connection = await mysql.createConnection(dbConfig);
       const [rows] = await connection.execute('SELECT * FROM catchments'); // Adjust query as needed
       connection.end();
     } else {
-      const rows = await anyQuery({
+      const [rows] = await anyQuery({
         tbl: 'catchments',
         select: '*'
       });
