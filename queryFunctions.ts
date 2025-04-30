8f7f261f37e5c50fb7e6f135e55da150f7a785dd
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2025-04-30 15:31:43 -0700

    retry

--- queryFunctions.ts
+++ queryFunctions.ts
@@ -94,4 +94,4 @@ export async function anyQuery({
   }
 }
 
-module.exports = { anyQuery }; 
+export default { anyQuery }; 
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import wellknown from 'wellknown';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-const { anyQuery } = require('./queryFunctions');
+import { anyQuery } from './queryFunctions.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
