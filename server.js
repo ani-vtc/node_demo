122a6898fb6569d67bb2ec4e60497682feaa8f59
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2025-04-30 14:24:10 -0700

    .js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import wellknown from 'wellknown';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-import { anyQuery } from './queryFunctions';
+import { anyQuery } from './queryFunctions.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
