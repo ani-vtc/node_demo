75a43646c8364b274e0c76e4b10cb4920d0c9b84
StrawberryMilsc <51016465+StrawberryMilsc@users.noreply.github.com>
2025-08-12 10:09:39 -0700

    branching

--- server.js
+++ server.js
@@ -218,6 +218,7 @@ class MCPClient {
             input_schema: tool.inputSchema,
           };
         });
+        this.tools.filter((tool) => tool.name !== "changeDatabase");
         console.log(
           "Connected to MCP server with tools:",
           this.tools.map(({ name }) => name)
